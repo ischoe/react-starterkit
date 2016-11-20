c57b1ca459662acb58b0a541af843b45539157be
ischoe <schoenberger.ingo@gmail.com>
2016-11-20 13:37:18 +0100

    replaced mocha with tape

--- src/index.test.js
+++ src/index.test.js
@@ -1,21 +1,31 @@
-import {expect} from 'chai';
+import test from 'tape';
 import jsdom from 'jsdom';
 import fs from 'fs';
 
-describe('Our first test', () => {
-		it('should pass ', () => {
-			expect(true).to.equal(true);
-		});
+test('A passing test', (assert) => {
+
+  assert.pass('This test will pass.');
+  assert.end();
 });
 
-describe('index.html', () => {
-		it('should say hello ', (done) => {
-			const index = fs.readFileSync('./src/index.html', 'utf-8');
-			jsdom.env(index, (err, window) => {
-					const h1 = window.document.getElementsByTagName('h1')[0];
-					expect(h1.innerHTML).to.equal('HELLO WORLD !');
-					done();
-					window.close();
-			});
-		});
+test('Assertions with tape.', (assert) => {
+  const expected = 'something to test';
+  const actual = 'something to test';
+
+  assert.equal(actual, expected,
+    'Given two mismatched values, .equal() should produce a nice bug report');
+  assert.end();
+});
+
+test('jsdom can be used', (assert) => {
+  const index = fs.readFileSync('./src/index.html', 'utf-8');
+  const expected = 'HELLO WORLD !';
+
+  jsdom.env(index, (err, window) => {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      assert.equal(h1.innerHTML, expected, 'h1 tag is correct');
+      assert.end();
+      window.close();
+  });
+
 });
