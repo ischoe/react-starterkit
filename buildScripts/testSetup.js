19321a6ce6727029e922aa554d596adb4bdae3d6
ischoe <schoenberger.ingo@gmail.com>
2016-11-27 09:33:08 +0100

    remove unused

--- buildScripts/testSetup.js
+++
@@ -1,7 +0,0 @@
-// This file isn't transpiled
-
-// Register babel to transpile before test run
-require('babel-register')();
-
-// Disable webpack features
-require.extensions['.css'] = function() {};
